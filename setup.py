df83c12a96b632d7d69d30a485b3e242219e31ed
Alberto Cetoli <alberto@contextscout.com>
2017-07-11 17:39:50 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='parvusdb',
-      version='0.0.2',
+      version='0.0.4',
       description='A lightweight in-memory graph database',
       url='http://github.com/fractalego/parvusdb',
       author='Alberto Cetoli',
       author_email='alberto@nlulite.com',
       license='MIT',
-      packages=['parvusdb'],
+      packages=['parvusdb', 'parvusdb.utils'],
       zip_safe=False)
